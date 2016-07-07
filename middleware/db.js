8da7f7a9874212f6de235aafcbbb050dcb4d8a28
MiSokol <so-mi-ig@yandex.ru>
2016-07-07 23:45:01 +0300

    added function to change info

--- middleware/db.js
+++ middleware/db.js
@@ -13,7 +13,7 @@ exports.get_user_by_login = function (login, callback) {
 		
 		if(!handleError) {return true;};
 		
-		console.log(login);
+		//console.log(login);
 		
 		//Choose rows with same login
 		var qstring = "SELECT * FROM users WHERE login = '" + login + "';"; 
@@ -25,7 +25,73 @@ exports.get_user_by_login = function (login, callback) {
 			cnt += 1;
 		});
 		
-		console.log("Rows count in db answer: ", cnt);
+		//console.log("Rows count in db answer: ", cnt);
+		
+		query.on('end', function (result) {
+			callback(result);
+		});
+		done();	
+	});
+};
+
+exports.change_user_password = function(login, new_user_pass, callback) {
+	pg.connect(db_url, function(err, client, done) {
+		var handleError = function (err) {
+            if(!err) return false;
+            done(client);
+            res.writeHead(500, {'content-type': 'text/plain'});
+            res.end('An error occurred');
+            return true;
+        };
+		
+		if(!handleError) {return true;};
+		
+		//console.log(login);
+		
+		//Choose rows with same login
+		var qstring = "UPDATE users SET pass = '" + new_user_pass + "WHERE login = '" + login + "';"; 
+		var query = client.query(qstring);
+		var cnt = 0;	
+		
+		query.on('row', function (row, result) {
+			result.addRow(row);
+			cnt += 1;
+		});
+		
+		//console.log("Rows count in db answer: ", cnt);
+		
+		query.on('end', function (result) {
+			callback(result);
+		});
+		done();	
+	});
+};
+
+exports.change_user_picture = function(login, new_user_pic, callback) {
+	pg.connect(db_url, function(err, client, done) {
+		var handleError = function (err) {
+            if(!err) return false;
+            done(client);
+            res.writeHead(500, {'content-type': 'text/plain'});
+            res.end('An error occurred');
+            return true;
+        };
+		
+		if(!handleError) {return true;};
+		
+		//console.log(login);
+		
+		//Choose rows with same login
+		var qstring = "UPDATE users SET userpic = '" + new_user_pic + "WHERE login = '" + login + "';"; 
+		var query = client.query(qstring);
+		var cnt = 0;	
+		
+		query.on('row', function (row, result) {
+			result.addRow(row);
+			cnt += 1;
+		});
+		
+		//console.log("Rows count in db answer: ", cnt);
 		
 		query.on('end', function (result) {
 			callback(result);
